3bcf280f6221c3b97fd6723aaebcd9ae8c7033e5
Thanh <dinhduythanh51292@gmail.com>
2015-07-16 13:29:55 +0700

    add entry post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
   root  'static_pages#home'
   get  'help'  =>  'static_pages#help'
@@ -10,6 +9,7 @@ Rails.application.routes.draw do
   post   'login'  => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
   resources :users
+   resources :entries,          only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
